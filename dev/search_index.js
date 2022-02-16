a8d83abf664c8d1bb18203aa4edc8bdf897f5cfd
Documenter.jl <documenter@juliadocs.github.io>
2022-02-16 10:56:20 +0000

    build based on 8eb4f71

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RayTracer","category":"page"},{"location":"#RayTracer","page":"Home","title":"RayTracer","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RayTracer.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RayTracer]","category":"page"}]
+}
